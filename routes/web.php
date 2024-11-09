4f8989d8687afc42c5a315e837803659b56d7bdc
Helal <122458084+amaritraj@users.noreply.github.com>
2024-11-09 18:45:40 +0600

    Web Route is Update

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,5 @@ Route::controller(ProductController::class)->group(function(){
     Route::get('/products/{product}/edit','edit')->name('products.edit');
     Route::put('/products/{product}','update')->name('products.update');
     Route::delete('/products/{product}','destroy')->name('products.destroy');
+    Route::get('/products/{product}','show')->name('products.show');
 });
